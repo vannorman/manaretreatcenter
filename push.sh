e23b819b55ca2d5d5df59d4e16326a7be19eb19c
Charlie Van Norman <charlie@vannorman.ai>
2023-05-03 21:03:51 -0500

    test

--- push.sh
+++ push.sh
@@ -1,6 +1,9 @@
 git add .
 git commit -m $1
 git push
-source ~/.ssh/main.sh & "cd manaretreat.center; git pull; sudo /etc/init.d/apache2 restart"
+source ~/.ssh/main.sh  
+cd manaretreat.center
+git pull
+sudo /etc/init.d/apache2 restart
 
 
