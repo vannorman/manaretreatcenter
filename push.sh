b560cd510ea7435080253a4bc242f657ab96cc3a
Charlie Van Norman <charlie@vannorman.ai>
2023-05-03 21:02:04 -0500

    test

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m $1
 git push
-source ~/.ssh/main.sh & "cd vannorman.ai; git pull; sudo /etc/init.d/apache2 restart"
+source ~/.ssh/main.sh & "cd manaretreat.center; git pull; sudo /etc/init.d/apache2 restart"
 
 
