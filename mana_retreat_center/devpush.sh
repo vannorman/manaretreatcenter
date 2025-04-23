336aabe317d728bce010231dff6fe87fe137ba83
Charlie Van Norman <charlie@vannorman.ai>
2025-04-23 18:32:52 -0500

    new site

--- mana_retreat_center/devpush.sh
+++
@@ -1,7 +0,0 @@
-git add .
-if [ -z "$1" ]; then echo "oops";
-else
-    git commit -a -m "$1"
-    git push
-    ssh -i ~/.ssh/fresh_cut_shirts.pem -t root@143.198.79.233 "cd /home/ubuntu/manaretreat.center/ ; git pull ; source /r.sh"
-fi
